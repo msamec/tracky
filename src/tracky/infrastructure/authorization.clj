ffde4c34d377ce67420df4079f8868f87f8a8e37
Marko Samec <marko@samec.dev>
2022-03-18 14:31:04 +0100

    feat: implement authentication and authorization

---
+++ src/tracky/infrastructure/authorization.clj
@@ -0,0 +1,22 @@
+(ns tracky.infrastructure.authorization
+  (:require [integrant.core :as ig]
+            [buddy.auth.accessrules :refer [wrap-access-rules success error]]
+            [ring.util.response :as resp]))
+
+(defn authenticated-access
+  [request]
+  (if (:identity request)
+    (success)
+    (error)))
+
+(def rules [{:pattern #"^(?!\/auth).*$"
+             :handler authenticated-access}])
+
+(defn on-error
+  [_request _value]
+  (resp/redirect "/auth/login"))
+
+(defmethod ig/init-key :tracky.infrastructure.authorization/access-rules [_ _]
+  (fn [handler]
+    (wrap-access-rules handler {:rules rules
+                                :on-error on-error})))
