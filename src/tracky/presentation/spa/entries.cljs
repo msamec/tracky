cc460ed905348deb2a8d2dd345c5a291e910c208
Marko Samec <marko@samec.dev>
2022-03-30 10:38:14 +0200

    feat(react): add loading overlay when clicking sync

--- src/tracky/presentation/spa/entries.cljs
+++ src/tracky/presentation/spa/entries.cljs
@@ -1,18 +1,20 @@
 (ns tracky.presentation.spa.entries
   (:require [tracky.presentation.spa.components.settings :refer [Settings]]
             [tracky.presentation.spa.components.table :refer [Table refresh]]
-            [tracky.presentation.spa.components.oauth2 :refer [OAuth2 authenticated]]))
+            [tracky.presentation.spa.components.oauth2 :refer [OAuth2 authenticated]]
+            [tracky.presentation.spa.components.loading :refer [Overlay]]))
 
 (defn Entries []
-  [:main
-   {:class "container mx-w-6xl mx-auto py-4"}
-   [:div
-    {:class "flex flex-col"}
-    (if-not (nil? @authenticated)
-      (do
-        (refresh)
-        [:<>
-         [Table]
-         [Settings]])
-      [OAuth2])]])
+  [Overlay
+   [:main
+    {:class "container mx-w-6xl mx-auto py-4"}
+    [:div
+     {:class "flex flex-col"}
+     (if-not (nil? @authenticated)
+       (do
+         (refresh)
+         [:<>
+          [Table]
+          [Settings]])
+       [OAuth2])]]])
 
